e1669daffb77eb5bc11018596b2e83ed7adaba3e
ProstoRAIN <64376319+ProstoRAIN@users.noreply.github.com>
2020-04-27 21:51:30 +0300

    Updates

---
+++ precache-manifest.fee99be2aae83a573496a320b4c5b4f0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2faa19164b8136ce9e44061db3e95499",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4fb803c1a5fa1a5d8b92",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b9364db56464d0e5170d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "4fb803c1a5fa1a5d8b92",
+    "url": "./static/js/2.bcf117d9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.bcf117d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9364db56464d0e5170d",
+    "url": "./static/js/main.217435c8.chunk.js"
+  },
+  {
+    "revision": "25872b2eea8e25fa13c3",
+    "url": "./static/js/runtime-main.f8832c52.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
