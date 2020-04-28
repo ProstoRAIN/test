7c5d8f4497350bd61e91aca6a56f00cdd7ae053c
ProstoRAIN <64376319+ProstoRAIN@users.noreply.github.com>
2020-04-28 13:26:39 +0300

    Updates

--- precache-manifest.7218e365f7ab9a4c12aa7b71a1170be6.js
+++ precache-manifest.7218e365f7ab9a4c12aa7b71a1170be6.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "27ce7bcc87c9d8f5e4048b4d31a731f4",
+    "revision": "125cbc5af793f509685653dbb0443225",
     "url": "./index.html"
   },
   {
-    "revision": "4fb803c1a5fa1a5d8b92",
+    "revision": "6add8216bfd617abe23a",
     "url": "./static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "b95726a167354f684d73",
-    "url": "./static/css/main.1cc385d6.chunk.css"
+    "revision": "39a435ef15ed11574145",
+    "url": "./static/css/main.981f7211.chunk.css"
   },
   {
-    "revision": "4fb803c1a5fa1a5d8b92",
-    "url": "./static/js/2.bcf117d9.chunk.js"
+    "revision": "6add8216bfd617abe23a",
+    "url": "./static/js/2.72a546d8.chunk.js"
   },
   {
     "revision": "21ced859ea2b2d6b856d461ad6c2afed",
-    "url": "./static/js/2.bcf117d9.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.72a546d8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b95726a167354f684d73",
-    "url": "./static/js/main.71df8190.chunk.js"
+    "revision": "39a435ef15ed11574145",
+    "url": "./static/js/main.14f3fc27.chunk.js"
   },
   {
     "revision": "25872b2eea8e25fa13c3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.9f4433f08e25dcdfa36a0f3ff60d28b6.js"
+  "./precache-manifest.7218e365f7ab9a4c12aa7b71a1170be6.js"
 );
 
 self.addEventListener('message', (event) => {
